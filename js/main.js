1a763b28176f30a0c2ddab8fa2ae882bdf93e1dc
JoevaScript1 <131012752+JoevaScript1@users.noreply.github.com>
2024-05-01 07:55:36 -0400

    Cleaned up Folders

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+function booleanToString(b){
+  if (b === true){
+    let b = "true"
+    return b
+  } else{
+    let b = "false"
+    return b
+  }
+}
