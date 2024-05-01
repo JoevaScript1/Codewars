ebdeaa8637aa4138a1711f542b1fa16f8069a935
JoevaScript1 <131012752+JoevaScript1@users.noreply.github.com>
2024-05-01 08:19:43 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,3 +11,13 @@ function booleanToString(b){
     return b
   }
 }
+
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+function between(a, b) {
+  let result = []
+  for(let i = a; i <= b; i++){ 
+    result.push(i)
+  }
+    return result
+}
