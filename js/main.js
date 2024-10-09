436a81e99f86aebca9ca2e133e036c14f12aa66f
JoevaScript1 <131012752+JoevaScript1@users.noreply.github.com>
2024-10-08 22:20:50 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -282,3 +282,23 @@ function bmi(weight, height) {
   }
   }
   
+
+//   Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+// Names given are always valid strings.
+
+
+function areYouPlayingBanjo(name) {
+  const nameSplit = name.split('');
+  if (nameSplit[0].toLowerCase() != 'r'){
+    return (`${name} does not play banjo`)
+  }
+  else{
+    return (`${name} plays banjo`)
+  }
+}
