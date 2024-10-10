0baf05eaa3c5d43faba4ac0432dc8e483eff0f85
JoevaScript1 <131012752+JoevaScript1@users.noreply.github.com>
2024-10-09 20:12:08 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -302,3 +302,20 @@ function areYouPlayingBanjo(name) {
     return (`${name} plays banjo`)
   }
 }
+
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+  let strArray = str.split('')
+  var vowels = 0
+  for (i=0; i<strArray.length; i++){
+    if(strArray[i] == 'a' || strArray[i] == 'e' || strArray[i] == 'i' || strArray[i] == 'o' || strArray[i] == 'u' ){
+      vowels += 1
+    }
+  }
+  return vowels
+}
