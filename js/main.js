f2170b49ec151f163b083e9b96986c9568c4c702
JoevaScript1 <131012752+JoevaScript1@users.noreply.github.com>
2024-11-13 18:30:04 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -292,3 +292,11 @@ function doubleChar(str) {
     .map((char) => char + char)
     .join("");
 }
+
+// Given an array of integers, find the one that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+function findOdd(a) {
+  return a.reduce((acc, num) => acc ^ num, 0);
+}
