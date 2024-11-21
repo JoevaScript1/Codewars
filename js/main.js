73e4536f182b591dae4a22f8f40f8d2ec6a36a98
JoevaScript1 <131012752+JoevaScript1@users.noreply.github.com>
2024-11-20 20:23:50 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -338,3 +338,29 @@ function stringy(size) {
   }
   return result; // Return the resulting string
 }
+
+function getDrinkByProfession(param) {
+  if (param.toLowerCase() === "jabroni") {
+    return "Patron Tequila";
+  }
+  if (param.toLowerCase() === "school counselor") {
+    return "Anything with Alcohol";
+  }
+  if (param.toLowerCase() === "programmer") {
+    return "Hipster Craft Beer";
+  }
+  if (param.toLowerCase() === "bike gang member") {
+    return "Moonshine";
+  }
+  if (param.toLowerCase() === "politician") {
+    return "Your tax dollars";
+  }
+  if (param.toLowerCase() === "rapper") {
+    return "Cristal";
+  }
+  if (param.toLowerCase() === "pundit") {
+    return "Beer";
+  } else {
+    return "Beer";
+  }
+}
