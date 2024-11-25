38ac2d6c0da3d540b65a7994530b7f17b32c4639
Joe <Jobesliani@gmail.com>
2024-11-25 07:32:56 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -366,3 +366,8 @@ function getDrinkByProfession(param) {
 }
 
 //Update
+
+function derive(coefficient,exponent) {
+  return (coefficient * exponent).toString() + "x^" + (exponent - 1).toString()
+  
+}
